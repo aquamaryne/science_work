ff9d48bac671ab06bd180e50055d21fa1b80fc09
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-08-21 10:54:20 +0300

    added front

--- main.py
+++ main.py
@@ -10,16 +10,24 @@ def number_convert(value):
     except ValueError:
         return float(value)
     
-def calculate_budget(Q1, Qt, Qmizh, Qias, Qlits, Qn, Qvp, Qupr, Qdpp):
+def calculate_qoz(Q1, Qt, Qmizh, Qias, Qlits, Qn, Qvp, Qupr, Qdpp):
     Qoz = Q1 - Qt - Qmizh - Qias - Qlits - Qn - Qvp - Qupr - Qdpp
     return Qoz
+
+def calculate_qmz(Q2, Qkred, Qias2, Qn2, Qdpp2, Qkom):
+    QMZ = Q2 - Qkred - Qias2 - Qn2 - Qdpp2 - Qkom
+    return QMZ
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/page2')
+def page2():
+    return render_template('index1.html')
+
 @app.route('/calculate', methods=['POST'])
-def calculate():
+def calculate_qoz_view():
     data = request.form
     Q1 = number_convert(data.get('Q1'))
     Qt = number_convert(data.get('Qt'))
@@ -31,9 +39,22 @@ def calculate():
     Qupr = number_convert(data.get('Qup'))
     Qdpp = number_convert(data.get('QDPP'))
 
-    Qoz = calculate_budget(Q1, Qt, Qmizh, Qias, Qlits, Qn, Qvp, Qupr, Qdpp)
-
+    Qoz = calculate_qoz(Q1, Qt, Qmizh, Qias, Qlits, Qn, Qvp, Qupr, Qdpp)
     return render_template('index.html', Qoz=Qoz)
+
+@app.route('/calculate1', methods=['POST'])
+def calculate_qmz_view():
+    data = request.form
+    Q2 = number_convert(data.get('Q2'))
+    Qkred = number_convert(data.get('Qkred'))
+    Qias2 = number_convert(data.get('Qias2'))
+    Qn2 = number_convert(data.get('Qn2'))
+    Qdpp2 = number_convert(data.get('Qdpp2'))
+    Qkom = number_convert(data.get('Qkom'))
+
+    QMZ = calculate_qmz(Q2, Qkred, Qias2, Qn2, Qdpp2, Qkom)
+    return render_template('index1.html', QMZ=QMZ)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
