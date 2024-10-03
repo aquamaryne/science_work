8ab3988e191550e2775b3af321919bfab87c1d4b
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-10-03 16:59:59 +0300

    updates

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import PageThree from './components/pageone';
 import MainPage from './components/mainpage';
-import Navbar from './components/navbar';
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar />
         <Routes>
           <Route path='/mainpage' element={ <MainPage /> } />
-          <Route path='/pageone' element={ <PageThree />} />
         </Routes>
       </Router>
 
--- react-app/src/components/mainpage.tsx
+++ react-app/src/components/mainpage.tsx
@@ -1,70 +1,70 @@
 import * as React from 'react';
-import { QmzFormData } from '../interface/qmz';
-import { QozFormData } from '../interface/qoz';
-import Qoz from './qozForm';
-import Qmz from './qmzForm';
-import { Container, Typography, Paper } from '@mui/material';
+import { Box, Typography, MenuItem, Select, FormControl, InputLabel, SelectChangeEvent } from "@mui/material";
+import city from '../interface/city';
+interface City {
+    id: number,
+    name: string,
+};
+
+const cities: City[] = [
+    { id: 1, name: 'Вінницька область' },
+    { id: 2, name: 'Волинська область' },
+    { id: 3, name: 'Дніпропетровська область' },
+    { id: 4, name: 'Донецька область' },
+    { id: 5, name: 'Житомирська область' }, 
+    { id: 6, name: 'Закарпатська область' },
+    { id: 7, name: 'Запорізька область' },
+    { id: 8, name: 'Івано-Франківська область' },
+    { id: 9, name: 'Київська область' },
+    { id: 10, name: 'Кіровоградська область' },
+    { id: 11, name: 'Луганська область' },
+    { id: 12, name: 'Львівська область' },
+    { id: 13, name: 'Миколаївська область' },
+    { id: 14, name: 'Одеська область' },
+    { id: 15, name: 'Полтавська область' },
+    { id: 16, name: 'Рівненська область' },
+    { id: 17, name: 'Сумська область' },
+    { id: 18, name: 'Тернопільська область' },
+    { id: 19, name: 'Харківська область' },
+    { id: 20, name: 'Херсонська область' },
+    { id: 21, name: 'Хмельницька область' },
+    { id: 22, name: 'Черкаська область' },
+    { id: 23, name: 'Чернігівська область' },
+    { id: 24, name: 'Чернівецька область' },
+    { id: 25, name: 'Автономна Республіка Крим' },
+];
 
 const MainPage: React.FC = () => {
-    const [qozResult, setQozResult] = React.useState<number | null>(null);
-    const [qmzResult, setQmzResult] = React.useState<number | null>(null);
+    const [selectedById, setSelectedById] = React.useState<number | null>(null);
 
-    const handleQozSubmit = async(formData: QozFormData) => {
-        const responce = await fetch('http://127.0.0.1:5000/calculate_qoz', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-        });
-
-        const data = await responce.json();
-        setQozResult(data.Qoz)
-    }
-
-    const handleQmzSubmit = async(formData: QmzFormData) => {
-        const responce = await fetch('http://127.0.0.1:5000/calculate_qmz', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-        });
-
-        const data = await responce.json();
-        setQmzResult(data.Qmz)
-    }
+    const handleCityChange = (e: SelectChangeEvent<string | number>) => {
+        const cityId = Number(e.target.value);
+        setSelectedById(cityId);
+    };
 
     return (
-        <Container maxWidth="sm" sx={{ mt: 4 }}>
-            <Typography variant="h4" gutterBottom>
-                Calculate QOZ
-            </Typography>
-            <Qoz onSubmit={handleQozSubmit} />
-
-            {qozResult !== null && (
-                <Paper elevation={3} sx={{ p: 2, mt: 2 }}>
-                    <Typography variant="h6" color="primary">
-                        QOZ Result:
-                    </Typography>
-                    <Typography variant="body1">{qozResult}</Typography>
-                </Paper>
+        <Box sx={{ maxWidth: 400, margin: '0 auto', padding: 2 }}>
+            <FormControl fullWidth variant='standard' sx={{ marginBottom: 2 }}>
+                <InputLabel>Оберіть область</InputLabel>
+                <Select
+                    value={selectedById || ''}
+                    onChange={handleCityChange}
+                    label='Оберіть область'
+                >
+                    {cities.map((city) => (
+                        <MenuItem key={city.id} value={city.id}>
+                            {city.name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            {selectedById !== null && (
+                <Box>
+                    <Typography variant='h6'></Typography>
+                    <Typography>{city[selectedById]}</Typography>
+                </Box>
             )}
-
-            <Typography variant="h4" gutterBottom sx={{ mt: 4 }}>
-                Calculate QMZ
-            </Typography>
-            <Qmz onSubmit={handleQmzSubmit} />
-
-            {qmzResult !== null && (
-                <Paper elevation={3} sx={{ p: 2, mt: 2 }}>
-                    <Typography variant="h6" color="secondary">
-                        QMZ Result:
-                    </Typography>
-                    <Typography variant="body1">{qmzResult}</Typography>
-                </Paper>
-            )}
-        </Container>
+        </Box>
     );
 }
 
